f991de7e5a1d55b1c94823166463f0363ae42012
Raul Boquin <boquin.raul.work@gmail.com>
2025-12-06 12:56:28 +0000

    test on_message

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import os
+import logging
+import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix="!")
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content.startswith("hello"):
+        await message.channel.send(f"hello {message.author.mention}")
+
+    await bot.process_commands(message)
+
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
